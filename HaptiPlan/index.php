d36eb0c3dd0bcee67d80189451574f3946fbafdd
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2023-12-05 16:30:09 +0100

    added documentation in models and added requires

--- HaptiPlan/index.php
+++ HaptiPlan/index.php
@@ -8,17 +8,17 @@ require_once './Router.php';
 require_once './request/Request.php';
 require_once './response/Response.php';
 require_once './response/ResponseHandler.php';
+require_once './models/decision.php';
+require_once './models/building.php';
+require_once './models/credit.php';
+require_once './models/employeeproduction.php';
+require_once './models/raw.php';
+require_once './models/machine.php';
+
 
 $request = new Request();
 $responseHandler = new ResponseHandler();
 
 $router = new Router('/haptiplan-backend/HaptiPlan/');
 $response = $router->callController($request);
-$responseHandler->sendResponse($response); 
-
-
-
-
-
-
-
+$responseHandler->sendResponse($response);
