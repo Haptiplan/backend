570628239ed7dd5bc53aa8ef99f731fbfc80f79f
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2024-03-05 15:32:59 +0100

    implemented autoload

--- HaptiPlan/index.php
+++ HaptiPlan/index.php
@@ -4,25 +4,26 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 Header("Access-Control-Allow-Methods: *");
 
-require_once './dao/dao.php';
-require_once './dao/machinedao.php';
-require_once './dao/creditdao.php';
-require_once './dao/buildingdao.php';
-
-require_once './include/database.php';
 
 require_once './router.php';
-require_once './request/request.php';
-require_once './response/response.php';
-require_once './response/responseHandler.php';
 
-require_once './models/decision.php';
-require_once './models/building.php';
-require_once './models/credit.php';
-require_once './models/employeeproduction.php';
-require_once './models/raw.php';
-require_once './models/machine.php';
-require_once './models/machinetype.php';
+spl_autoload_register(function ($class) {
+    $directories = [
+        __DIR__ . '/dao/',
+        __DIR__ . '/response/',
+        __DIR__ . '/include/',
+        __DIR__ . '/request/'
+    ];
+
+    foreach ($directories as $directory) {
+        $classPath = $directory . strtolower($class) . '.php';
+        if (file_exists($classPath)) {
+            require_once $classPath;
+            return;
+        }
+    }
+});
+
 
 $request = new Request();
 $responseHandler = new ResponseHandler();
@@ -30,10 +31,3 @@ $router = new Router('/backend/HaptiPlan/');
 $response = $router->callController($request);
 
 $responseHandler->sendResponse($response); 
-
-
-
-
-
-
-
