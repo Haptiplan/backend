860a4f0398811e03d0067e5822f6645f69978ba6
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2024-05-22 14:59:37 +0200

    created 3 different users

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,36 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/**General user routes **/
+Route::middleware(['auth', 'verified'])->get('/dashboard', [DashboardController::class, 'generalUserDashboard'])->name('dashboard');
+
+/**Admin routes **/
+Route::middleware('adminAuth')->prefix('admin')->group(function(){
+    Route::get('/dashboard', [DashboardController::class, 'adminDashboard'])->name('adminDashboardShow');
+});
+
+/**Gamemaster routes **/
+Route::middleware('gamemasterAuth')->prefix('gamemaster')->group(function(){
+    Route::get('/dashboard', [DashboardController::class, 'gamemasterDashboard'])->name('gamemasterDashboardShow');
 });
 
 require __DIR__.'/auth.php';
