2d2e8e1aa3e2651375892a4b5ff987ddce723514
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-07-04 15:28:45 +0200

    fixed quotation marks

--- resources/views/machines.blade.php
+++ resources/views/machines.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
 
-    <h1 class="text-2xl font-bold mb-6>Machine List</h1>
-<div class="py-12">
+    <h1 class="text-2xl font-bold mb-6">Machine List</h1>
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
