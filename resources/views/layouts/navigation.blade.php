e2a51fa695c2fb61abdcc3ec8be3c921f6b8d8e3
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-07-19 11:13:31 +0200

    Navbar updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                         
                     </x-nav-link>
-                    @if(Auth::check() && (Auth::user()->role == \App\Models\User::ROLE_ADMIN || Auth::user()->role == \App\Models\User::ROLE_GAMEMASTER))
+                    @if(Auth::check() && Auth::user()->role == \App\Models\User::ROLE_GAMEMASTER)
                     <x-nav-link :href="route('game_index')" :active="request()->routeIs('game_index')">
                         {{ __('Game erstellen') }}
                     </x-nav-link>
