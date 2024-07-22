cf234632c51746983c6a88e3986a45f42df8f1d8
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-07-22 15:40:38 +0200

    Company name only needs to be unique in selected game

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,6 +7,7 @@ use App\Models\Game;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Closure;
 
 class CompanyController extends Controller
 {
@@ -33,9 +34,21 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
+        $company_name = $request->input('company_name');
+        $game_id_input = $request->input('game_id');
+
+        $company = Company::where('company_name', $company_name)->get();
+        $company_ids = $company->pluck('game_id')->toArray();
+
+        $game_id_exists = $company && in_array($game_id_input, $company_ids);
         $validated = $request->validate([
-            'company_name' => 'required|unique:companies,company_name',
-            'game_id' => 'required',
+            'company_name' => 'required',
+            'game_id' => [function (string $attribute, mixed $value, Closure $fail) use ($game_id_exists)
+            {
+                if ($game_id_exists) {
+                    $fail("The name of the company must be unique in the selected game!");
+                }
+            }],
         ]);
     
         $company_name = $validated['company_name'];
@@ -73,13 +86,26 @@ class CompanyController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $company_name = $request->input('company_name');
+        $game_id_input = $request->input('game_id');
+
+        $company = Company::where('company_name', $company_name)->get();
+        $company_ids = $company->pluck('game_id')->toArray();
+
+        $game_id_exists = $company && in_array($game_id_input, $company_ids);
         $validated = $request->validate([
-            'company_name' => 'required|unique:companies,company_name,' . $id,
-            'game_id' => 'required|exists:games,id', 
+            'company_name' => 'required',
+            'game_id' => [function (string $attribute, mixed $value, Closure $fail) use ($game_id_exists)
+            {
+                if ($game_id_exists) {
+                    $fail("The name of the company must be unique in the selected game!");
+                }
+            }],
         ]);
+        
         $company = Company::find($id);
         $company->company_name = $validated['company_name'];
-        $company->game_id = $request['game_id'];
+        $company->game_id = $validated['game_id'];
         $company->update();
 
         return redirect()->route('company_create');
