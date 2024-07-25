a07a739c381a37b7c7dc4e05c2b42ab8ee091098
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2024-07-25 13:37:48 +0200

    hotfix machine list for user

--- resources/views/machines.blade.php
+++ resources/views/machines.blade.php
@@ -5,20 +5,20 @@
         </h2>
     </x-slot>
 
-    <h1 class="text-2xl font-bold mb-6">Machine List</h1>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-2xl font-bold mb-6">Machines:</h1>
-                    <div class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                    <h1 class="text-2xl font-bold mb-6">Machinen Liste</h1>
+                    <div>
                         @foreach($machines as $machine)
-                             <li>{{$machine->machineName }}</li>
-                         @endforeach
+                        <li>
+                            {{$machine->machine_name}}
+                        </li>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
 </x-app-layout>
