11aa0e47a9cc5503b547c4c45ca5b5d4bb5323f5
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-09-11 11:43:06 +0200

    Updated seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,12 +27,26 @@ class UserSeeder extends Seeder
         $userObj->role = $user;
         $userObj->save();
 
+        $userObj2 = new User();
+        $userObj2->name = 'Test User';
+        $userObj2->email = 'test@test.com';
+        $userObj2->password = Hash::make('123');
+        $userObj2->role = $user;
+        $userObj2->save();
+
         $gamemasterObj = new User();
         $gamemasterObj->name = 'Gamemaster Hicham';
         $gamemasterObj->email = 'gh@gmail.com';
         $gamemasterObj->password = Hash::make('123');
         $gamemasterObj->role = $gamemaster;
         $gamemasterObj->save();
+
+        $gamemasterObj2 = new User();
+        $gamemasterObj2->name = 'Test Gamemaster';
+        $gamemasterObj2->email = 'gt@test.com';
+        $gamemasterObj2->password = Hash::make('123');
+        $gamemasterObj2->role = $gamemaster;
+        $gamemasterObj2->save();
 
         $adminObj = new User();
         $adminObj->name = 'Admin Hicham';
