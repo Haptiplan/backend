4aefe2737f6cd63f8d22c3b38b0377560379fd35
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2024-09-25 16:32:42 +0200

    Decision is restricted to active games

--- resources/views/decisions/check.blade.php
+++ resources/views/decisions/check.blade.php
@@ -8,7 +8,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-2xl font-bold mb-6">
+                    @if ($game->active == 1)
+                    <h1 class="text-2xl font-bold mb-6"> 
                         {{ __('messages.decisionName') . " " . $period . " " . __('messages.fromGame') . " " . $game->name }}
                     </h1>
                     <div>
@@ -51,6 +52,7 @@
                             </ul>
                         </div>
                         @endif
+                    @endif    
                         <!--table>
                             Maybe a table to change period and game from view
                         </table-->
