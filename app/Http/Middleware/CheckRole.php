fddb85d7688804d3c10c2df6bea39bc9edd710db
Omar kanoune <38401772+omarKanoune@users.noreply.github.com>
2025-01-06 16:07:05 +0100

    I have added a relationship between the User and Role tables, and I am using it in the code

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -13,13 +13,13 @@ class CheckRole
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Closure(Request): (Response)  $next
      */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        if (Auth::check() && in_array(Auth::user()->role, $roles)) {
+        if (Auth::check() && in_array(Auth::user()->role->name, $roles)) {
             return $next($request);
-        } 
+        }
         return response()->json(['error' => 'Unauthorized'], 403);
     }
 }
