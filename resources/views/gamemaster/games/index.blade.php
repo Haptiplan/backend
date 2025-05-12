e38fa1b98ac25b8c1009f481b42b45c2f83fc66a
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2025-05-12 09:46:53 +0200

    improved gamemaster ui

--- resources/views/gamemaster/games/index.blade.php
+++ resources/views/gamemaster/games/index.blade.php
@@ -22,7 +22,6 @@
                         </div>
                     @endif
                     <x-success-message></x-success-message>
-                    <x-create-button href="{{ route('games.create') }}">{{ __('messages.gameCreate') }}</x-create-button>
                     <div>
                         @foreach($games as $game)
                         <li>
