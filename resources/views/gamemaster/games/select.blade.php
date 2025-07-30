cf5cf74a9f84b17b2824a31ead013dc6b05c1aae
HichamElBadr <hicham-elbadr@hotmail.de>
2025-07-30 14:23:16 +0200

    added messages

--- resources/views/gamemaster/games/select.blade.php
+++ resources/views/gamemaster/games/select.blade.php
@@ -1,20 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            Spiel auswählen
+            {{ __('messages.selectGame')  }}
         </h2>
     </x-slot>
 
     <div class="p-6">
         <div class="mt-4 space-y-2">
             @foreach($games as $game)
-                <form method="POST" action="{{ route('games.set_selected', $game) }}">
-                    @csrf
-                    <button class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                        {{ $game->name }}
-                    </button>
-                </form>
+            <form method="POST" action="{{ route('games.set_selected', $game) }}">
+                @csrf
+                <button class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                    {{ $game->name }}
+                </button>
+            </form>
             @endforeach
         </div>
     </div>
+    <div class="text-center mb-6">
+        <x-create-button href="{{ route('games.create') }}" class="px-6 py-3 bg-gradient-to-r from-blue-500 to-teal-400 text-white rounded-full text-lg font-semibold hover:from-teal-400 hover:to-blue-500 transition duration-300 ease-in-out transform hover:scale-105 shadow-lg hover:shadow-xl">
+            {{ __('messages.gameCreate') }}
+        </x-create-button>
+    </div>
+
 </x-app-layout>
